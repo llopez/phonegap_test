8a8bef6d65587522f4c9f48d46915c80ab172516
Luis Lopez <luigibyte@gmail.com>
2016-03-14 14:31:33 -0300

    xxx

--- www/js/index.js
+++ www/js/index.js
@@ -37,12 +37,16 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
+        navigator.splashscreen.show();
+
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
+
+        navigator.splashscreen.hide();
 
         console.log('Received Event: ' + id);
     }
